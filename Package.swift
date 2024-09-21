958cc0692ebb4a653afeef8b2b9b88c12db571a2
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2024-09-21 22:20:59 +0900

    rename library to VcWalletLibrary

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "vc-wallet-ios-library",
+    name: "VcWalletLibrary",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "vc-wallet-ios-library",
-            targets: ["vc-wallet-ios-library"]),
+            name: "VcWalletLibrary",
+            targets: ["VcWalletLibrary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "vc-wallet-ios-library"),
+            name: "VcWalletLibrary"),
         .testTarget(
-            name: "vc-wallet-ios-libraryTests",
-            dependencies: ["vc-wallet-ios-library"]),
+            name: "VcWalletLibraryTests",
+            dependencies: ["VcWalletLibrary"]),
     ]
 )
